5f3353e0603460f97257ec60006c70d94e0d519b
mathan pon singh <mathanponsingh@gmail.com>
2026-07-30 14:25:41 +0530

    Added backend code for fetching all the data from the db

---
+++ backend/routes/router.py
@@ -0,0 +1,17 @@
+from fastapi import APIRouter
+from config.databse import reuters_technology_collections
+
+router = APIRouter(
+    prefix="/news",
+    tags=["Reuters Technology"]
+)
+
+
+@router.get("/technology")
+def get_reuters_technology_news():
+    data = list(reuters_technology_collections.find({}))
+    for item in data:
+        # The _id is an ObjectId type so I changed this type to string 
+        if "_id" in item:
+            item["_id"] = str(item["_id"])
+    return data
