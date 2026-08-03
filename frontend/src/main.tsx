378e1f30d4502f5b0dbb353b873432fbaa2b409a
Paul-isol <isolpaul@gmail.com>
2026-08-03 11:59:11 +0530

    oAuth implemented

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,9 +2,14 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { BrowserRouter } from 'react-router-dom'
+import { ToastContainer } from 'react-toastify'
 
 createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
     <App />
+    <ToastContainer 
+    position='top-right'
+    autoClose={3000}
+    />
   </BrowserRouter>,
 )
